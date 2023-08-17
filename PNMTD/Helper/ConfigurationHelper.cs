bad9ae99cf39ef1e3efbe94439485bda27b072da
alptbz <info@uisa.ch>
2023-08-17 12:42:49 +0200

    fixed testing detail

--- PNMTD/Helper/ConfigurationHelper.cs
+++ PNMTD/Helper/ConfigurationHelper.cs
@@ -9,7 +9,7 @@ namespace PNMTD.Helper
         {
             var config = new ConfigurationBuilder()
                .AddJsonFile("appsettings.Development.json")
-               .AddUserSecrets(Assembly.GetCallingAssembly(), false)
+               .AddUserSecrets(Assembly.GetCallingAssembly(), true)
                .AddEnvironmentVariables()
                .Build();
             return config;
